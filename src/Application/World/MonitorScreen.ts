0ebc8b44363d47bc3bec94f466c0de5da1d7357d
Zachary Dorf <88392270+inveve@users.noreply.github.com>
2023-02-26 22:59:06 -0500

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://globe.dorf.nyc';
+        iframe.src = 'https://inveve.com';
         /**
          * Use dev server is query params are present
          *

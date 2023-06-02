af55dc498318993d204543c95d75245354bb4219
Zachary Dorf <88392270+inveve@users.noreply.github.com>
2023-06-02 11:27:06 -0400

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://inveve.com';
+        iframe.src = 'https://bask.health';
         /**
          * Use dev server is query params are present
          *
